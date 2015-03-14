e759962d54afd6078cf9203433b9c59d25ebc946
dlwelch <dlwelch@users.noreply.github.com>
2015-03-13 21:51:52 -0500

    Update jQMSelectFilterPro.js

--- jQMSelectFilterPro.js
+++ jQMSelectFilterPro.js
@@ -39,11 +39,7 @@ $.widget("custom.jqmselectfilterpro", {
         }
     },
     /*
-    selects a specified single item passed in an array
-     i.e. [{"one": "1", "two" : "2 ", ... }]
-     since we are using _compareObjectKeys function
-     remember, we really don't care about the value of the select
-     so no need to fire the select event
+    selects a specified single item passed in an array i.e. [{"one": "1", "two" : "2 ", ... }]
    */
     selectitem: function (item) {
         if ($.isArray(item)) {
@@ -52,7 +48,7 @@ $.widget("custom.jqmselectfilterpro", {
                 return _that._compareObjectKeys(element, item);
             });
             if (myItem.length > 0) {
-                this.element.val(myItem[0].label);
+                this.element.val(myItem[0].jqmselectfilterprovalue);
                 $(this.element).find("option:first").html(myItem[0].label);
                 this._selectedvalue = myItem[0];
             }
@@ -66,7 +62,7 @@ $.widget("custom.jqmselectfilterpro", {
     _create: function () {
         var _that = this;
 
-        //add the prompt/title option
+        //add the first option to use as custom selectmenu placeholder
         var promptoption = document.createElement("option");
         $(promptoption).html(_that.options.searchprompt);
         _that.element.append(promptoption);
@@ -76,6 +72,12 @@ $.widget("custom.jqmselectfilterpro", {
         var currentCategory = "";
         var currentCategoryOptgroup;
         $.each(_that.options.source.items, function (index, item) {
+
+            //use this to sync up the source items and the select options 
+            // i.e.select.val() = item.jqmselectfilterprovalue
+            // +1 since the first option (placeholder) has a listview data-option-index of 0
+            item["jqmselectfilterprovalue"] = parseInt(index)+1;
+
             if (item["category"] != currentCategory && _that.options.categoryfield != null) {
                 currentCategoryOptgroup = document.createElement("optgroup");
                 $(currentCategoryOptgroup).attr("label", item["category"]);
@@ -84,7 +86,7 @@ $.widget("custom.jqmselectfilterpro", {
             }
 
             var thisoption = document.createElement("option");
-            $(thisoption).attr("value", index)
+            $(thisoption).attr("value", parseInt(index) + 1)
                 .html(item["label"])
                 .attr("data-filtertext", _that._getfiltertext(item))
                 .addClass("jqmselectfilterprosearchable");
@@ -120,20 +122,12 @@ $.widget("custom.jqmselectfilterpro", {
             _that.element.on("change", function (event) {
                 //set this jqmselectfilterpro's current value
                 _that._selectedvalue = _that.options.source.items[selectmenu.find(":selected").attr("value")];
-                //update the select element's first option to refect the current selection
-                //so it will be used as the title while filtering
+                //update the select's placeholder option to reflect the current selection 
                 $(this).find("option:first").html(_that._selectedvalue.label);
+                //update the popup title
+                $("#" + _that.element.attr("id") + "-listbox").find("h1").html(_that._selectedvalue.label);
             });
 
-            //The following code is used when selectmenu opens a popup instead of a dialog (page)
-            $("#" + selectmenu.attr("id") + "-listbox").on("popupbeforeposition", function (event, uiempty) {
-                //update the title of the popup to the currently selected label
-                $(this).find("h1").html(selectmenu.find(":selected").text());
-                // if using option categories, un-highlight the li since it won't follow correctly (the jquery selectmenu filterable doesn't do it right...)
-                if (_that.options.categoryfield != null)
-                    $(this).find("ul").find("[aria-selected='true']").attr("aria-selected", false).find("a").removeClass("ui-btn-active");
-
-            });
             //set the focus to the input search box in the popup when it opens
             $("#" + _that.element.attr("id") + "-listbox-popup").on("popupafteropen", function () {
                 $('input').focus();
@@ -166,9 +160,6 @@ $.widget("custom.jqmselectfilterpro", {
                         // FROM jQM demo: "Place the form before the listview in the dialog."
                         listview.before(form);
 
-                        // un-highlight the li since it won't follow correctly if using option categories (the jquery selectmenu filterable doesn't do it right...)
-                        if (_that.options.categoryfield != null)
-                            listview.find("li[aria-selected='true']").attr("aria-selected", false).find("a").removeClass("ui-btn-active");
                         //update the title of the dialog to the currently selected label
                         data.toPage.find("div.ui-title").html($("#" + _that.element.attr("id")).find(":selected").text());
                     }
@@ -197,15 +188,10 @@ $.widget("custom.jqmselectfilterpro", {
                 //but we will only search the options with the right class
                 children: ".jqmselectfilterprosearchable",
                 filterCallback: function (index, searchValue) {
-                    ////this is the normal default filterCallback which returns true to hide the current item
-                    //return (("" + ($.mobile.getAttribute(this, "filtertext") || $(this).text())).toLowerCase().indexOf(searchValue) === -1);
-
-                    //but we are going to customize it so we'll only show maxitems if that option has been set > 0
-                    //and to do a filterReveal if that option is set to true
-
                     //if we've reached our maxitems limit
                     //or if we have no search term (reveal) then hide this item
-                    if (((_that.options.maxitems > 0) && (_that._visibleitems >= _that.options.maxitems)) || (_that.options.filterReveal && searchValue == "")) {
+                    if (((_that.options.maxitems > 0) && (_that._visibleitems >= _that.options.maxitems))
+                        || (_that.options.filterReveal && searchValue == "")) {
                         return true;
                     } else {
                         var hide = (("" + ($.mobile.getAttribute(this, "filtertext") || $(this).text())).toLowerCase().indexOf(searchValue) === -1);
@@ -220,31 +206,26 @@ $.widget("custom.jqmselectfilterpro", {
                 //reset the current number of visible items
                 _that._visibleitems = 0;
             }).on("filterablefilter", function () {
-                // un-highlight the li since it won't follow correctly if using option categories (the jquery selectmenu filterable doesn't do it right...)
-                if (_that.options.categoryfield != null) {
-                    $("#" + selectmenu.attr("id") + "-listbox").find("li[aria-selected='true']").attr("aria-selected", false).find("a").removeClass("ui-btn-active");
-                    $("#" + _that.element.attr("id") + "-dialog").find("li[aria-selected='true']").attr("aria-selected", false).find("a").removeClass("ui-btn-active");
-                }
+                // correctly highlight the currently selected link button  
+                //(the jquery selectmenu filterable doesn't do it right..imho..)
+                var currentindex = parseInt(_that.element.find(":selected").attr("value"));
+                $("#" + _that.element.attr("id") + "-listbox").find("li[aria-selected='true']").attr("aria-selected", false).find("a").removeClass("ui-btn-active");
+                $("#" + _that.element.attr("id") + "-listbox").find("li[data-option-index='" + currentindex + "']").attr("aria-selected", true).find("a").addClass("ui-btn-active");
+
+                //this will not be needed in jQM 1.5
+                $("#" + _that.element.attr("id") + "-dialog").find("li[aria-selected='true']").attr("aria-selected", false).find("a").removeClass("ui-btn-active");
+                $("#" + _that.element.attr("id") + "-dialog").find("li[data-option-index='" + currentindex + "']").attr("aria-selected", true).find("a").addClass("ui-btn-active");
             });
-
         });
-
         this.element.selectmenu();
-        
     },
     _getfiltertext: function (element) {
-
         var _matchterm = "";
         $.each(this.options.searchfields, function (i, e) {
             _matchterm += element[e];
         });
         return _matchterm;
     },
-    /*
-    compares an object to each object in an array of objects
-    returns true if the object contains a matched set of elements from
-    any of the objects in the array - used for filtering
-    */
     _compareObjectKeys: function (element, set) {
         var filterlength = set.length;
         for (var i = 0; i < filterlength; i++) {
